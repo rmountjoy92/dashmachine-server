50d47266eab137a155035e3bd3bfcb002eebe17e
ross <ross@mountjoy.tech>
2021-10-22 09:27:30 -0400

    1

--- app/api/core/__init__.py
+++ app/api/core/__init__.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from app import dm
 
 core = Blueprint("core", __name__)
 
@@ -6,3 +7,13 @@ core = Blueprint("core", __name__)
 @core.route("/register_client", methods=["GET"])
 def register_client():
     return jsonify("success")
+
+
+@core.route("/config", methods=["GET"])
+def config():
+    return jsonify(dm.config_json)
+
+
+@core.route("/update_config", methods=["POST"])
+def update_config():
+    return jsonify(dm.modify_json(k=request.json.get("k"), v=request.json.get("v")))
--- app/dashmachine/__init__.py
+++ app/dashmachine/__init__.py
@@ -5,6 +5,8 @@ from app.paths import config_json
 class DashMachine:
     def __init__(self):
         self.app = None
+
+        # Set default configuration
         self.users = [{"username": "admin", "password": "admin", "role": "admin"}]
         self.settings = {
             "theme": {
@@ -16,15 +18,45 @@ class DashMachine:
             },
             "roles": ["admin"],
         }
-        self.dashboards = []
+        self.dashboards = [
+            {
+                "name": "DashMachine",
+            }
+        ]
         self.plugins = []
 
+        # Overwrite default configuration with config.json
+        self.config_json = {}
+        self.apply_config()
+
+        # write defaults to config json on first run
+        if self.config_json == {}:
+            self.modify_json("settings", self.settings)
+            self.modify_json("users", self.users)
+            self.modify_json("dashboards", self.dashboards)
+            self.modify_json("plugins", self.plugins)
+
+        print("DashMachine Initialized.")
+
+    def apply_config(self):
         with open(config_json) as f:
             self.config_json = json.loads(f.read() or "{}")
-
         for k, v in self.config_json.items():
             setattr(self, k, v)
-        print("DashMachine Initialized.")
+
+    def modify_json(self, k, v):
+        with open(config_json) as f:
+            data = json.loads(f.read() or "{}")
+
+        data[k] = v
+
+        with open(config_json, "w") as f:
+            json.dump(data, f, indent=6)
+        self.config_json = data
+        for key, val in self.config_json.items():
+            setattr(self, key, val)
+
+        return getattr(self, k)
 
     def init_app(self, app):
         self.app = app
